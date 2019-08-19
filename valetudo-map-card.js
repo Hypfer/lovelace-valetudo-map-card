1c974a29c00f1a611dd2c418701bbe285c037cd6
Sylvia van Os <sylvia@hackerchick.me>
2019-08-19 09:34:26 +0200

    Fix path drawing

--- valetudo-map-card.js
+++ valetudo-map-card.js
@@ -10,33 +10,74 @@ class ValetudoMapCard extends HTMLElement {
       container.firstChild.remove();
     };
 
-    const canvas = document.createElement('canvas');
-    canvas.width = mapData.attributes.image.dimensions.width;
-    canvas.height = mapData.attributes.image.dimensions.height;
+    const containerContainer = document.createElement('div');
+    const containerContainerStyle = document.createElement('style');
+    containerContainerStyle.textContent = `
+      div {
+        position: relative;
+        width: ${mapData.attributes.image.dimensions.width}px;
+        height: ${mapData.attributes.image.dimensions.height}px;
+      }
+      div canvas {
+        position: absolute;
+        background-color: transparent;
+      }
+    `
+    container.appendChild(containerContainer);
+    container.appendChild(containerContainerStyle);
 
-    container.appendChild(canvas);
+    const mapCanvas = document.createElement('canvas');
+    mapCanvas.width = mapData.attributes.image.dimensions.width;
+    mapCanvas.height = mapData.attributes.image.dimensions.height;
+    mapCanvas.style.zIndex = 1;
 
-    const ctx = canvas.getContext("2d");
-    ctx.fillStyle = this._config.floor_color;
+    const pathCanvas = document.createElement('canvas');
+    pathCanvas.width = mapData.attributes.image.dimensions.width;
+    pathCanvas.height = mapData.attributes.image.dimensions.height;
+    pathCanvas.style.zIndex = 2;
+
+    containerContainer.appendChild(mapCanvas);
+    containerContainer.appendChild(pathCanvas);
+
+    const mapCtx = mapCanvas.getContext("2d");
+    mapCtx.fillStyle = this._config.floor_color;
     for (let item of mapData.attributes.image.pixels.floor) {
-      ctx.fillRect(item[0], item[1], 1, 1);
+      mapCtx.fillRect(item[0], item[1], 1, 1);
     };
 
-    ctx.fillStyle = this._config.obstacle_weak_color;
+    mapCtx.fillStyle = this._config.obstacle_weak_color;
     for (let item of mapData.attributes.image.pixels.obstacle_weak) {
-      ctx.fillRect(item[0], item[1], 1, 1);
+      mapCtx.fillRect(item[0], item[1], 1, 1);
     };
 
-    ctx.fillStyle = this._config.obstacle_strong_color;
+    mapCtx.fillStyle = this._config.obstacle_strong_color;
     for (let item of mapData.attributes.image.pixels.obstacle_strong) {
-      ctx.fillRect(item[0], item[1], 1, 1);
+      mapCtx.fillRect(item[0], item[1], 1, 1);
     };
 
     // TODO: Charger and vacuum
 
-    ctx.fillStyle = this._config.path_color;
-    for (let item of mapData.attributes.path.points) {
-      ctx.fillRect(item[0], item[1], 1, 1);
+    if (mapData.attributes.path.points) {
+      const pathCtx = pathCanvas.getContext("2d");
+      const widthScale = 50;
+      const heightScale = 50;
+      const leftOffset = mapData.attributes.image.position.left;
+      const topOffset = mapData.attributes.image.position.top;
+      pathCtx.strokeStyle = this._config.path_color;
+
+      let first = true;
+      pathCtx.beginPath();
+      for (let item of mapData.attributes.path.points) {
+        let x = Math.floor((item[0]) / widthScale) - leftOffset;
+        let y = Math.floor((item[1]) / heightScale) - topOffset;
+        if (first) {
+          pathCtx.moveTo(x, y);
+          first = false;
+        } else {
+          pathCtx.lineTo(x, y);
+        };
+      };
+      pathCtx.stroke();
     };
   };
 
