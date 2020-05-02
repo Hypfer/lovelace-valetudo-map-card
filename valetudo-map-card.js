eb06596018a8c3ff6587f9429a68f79e990c002f
Sylvia van Os <sylvia@hackerchick.me>
2020-05-02 22:18:56 +0200

    Redraw map on theme change

--- valetudo-map-card.js
+++ valetudo-map-card.js
@@ -4,6 +4,7 @@ class ValetudoMapCard extends HTMLElement {
     this.drawingMap = false;
     this.drawingControls = false;
     this.lastUpdatedMap = "";
+    this.lastDrawnTheme = null;
     this.lastUpdatedControls = "";
     this.attachShadow({ mode: 'open' });
     this.lastValidRobotPosition = [];
@@ -42,8 +43,8 @@ class ValetudoMapCard extends HTMLElement {
     this.cardContainer.appendChild(this.controlContainerStyle);
   };
 
-  shouldDrawMap(state) {
-    return !this.drawingMap && this.lastUpdatedMap != state.last_updated;
+  shouldDrawMap(state, theme) {
+    return !this.drawingMap && (this.lastUpdatedMap != state.last_updated || this.lastDrawnTheme != theme);
   };
 
   shouldDrawControls(state) {
@@ -372,7 +373,7 @@ class ValetudoMapCard extends HTMLElement {
       const vacuumColor = this.calculateColor(homeAssistant, this._config.vacuum_color, '--primary-text-color');
 
       // Don't redraw unnecessarily often
-      if (this.shouldDrawMap(mapEntity)) {
+      if (this.shouldDrawMap(mapEntity, hass.selectedTheme)) {
         // Start drawing map
         this.drawingMap = true;
 
@@ -380,6 +381,7 @@ class ValetudoMapCard extends HTMLElement {
 
         // Done drawing map
         this.lastUpdatedMap = mapEntity.last_updated;
+        this.lastDrawnTheme = hass.selectedTheme;
         this.drawingMap = false;
       };
     };
