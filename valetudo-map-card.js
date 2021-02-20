fdbc594cb0cbf3c21708682c70e0528d06ac6fd1
Sylvia van Os <sylvia@hackerchick.me>
2021-02-20 22:19:27 +0100

    Default pollInterval to cleaning state
    So the map doesn't update only once every 2 minutes if there is no vacuum_entity defined

--- valetudo-map-card.js
+++ valetudo-map-card.js
@@ -8,7 +8,7 @@ class ValetudoMapCard extends HTMLElement {
     this.lastMapPoll = new Date(0);
     this.isPollingMap = false;
     this.lastRobotState = "docked";
-    this.pollInterval = POLL_INTERVAL_STATE_MAP["docked"];
+    this.pollInterval = POLL_INTERVAL_STATE_MAP["cleaning"];
 
 
     this.cardContainer = document.createElement('ha-card');
